b1ac428abc1c74e46c65e6ab426bb3f5a431af07
developer <1816579@gmail.com>
2023-10-22 01:46:34 +0300

    upd: markup and iframe embed (not working though)

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,27 @@
+var player;
 
+window.onYouTubeIframeAPIReady = function () {
+  console.log(YT);
+  player = new YT.Player('ytplayer', {
+    events: {
+      'onReady': onPlayerReady,
+    }
+  });
+}
+
+function onPlayerReady(event) {
+  console.log(event);
+  playVideo();
+}
+
+function playVideo() {
+  player.playVideo();
+}
+
+function pauseVideo() {
+  player.pauseVideo();
+}
+
+function stopVideo() {
+  player.stopVideo();
+}
